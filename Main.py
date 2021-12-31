ed1f49b9f96e4a3965bf4c4aa5ac13969f9970bd
Dhiness Karthikeyan <89132554+Dhinessplayz@users.noreply.github.com>
2021-12-31 11:08:30 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+import music
+
+cogs = [music]
+
+
+
+client = commands.Bot(command_prefix='m!', intents = discord.Intents.all())
+
+for i in range(len(cogs)):
+  cogs[i].setup(client)
+
+
+client.run('Your own discord bot token')
